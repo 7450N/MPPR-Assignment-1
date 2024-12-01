db18807be32ca1f72d64d3ba4ab2fd72fca59aae
7450N <minthawkaung10@gmail.com>
2024-12-02 03:45:47 +0800

    loading screen

--- MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/Interpolation.cs
+++ MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/Interpolation.cs
@@ -42,4 +42,34 @@ public class Interpolation : MonoBehaviour
         Vector3 interpolatedPos = (1 - t) * posA + t * posB;
         return interpolatedPos;                             //return the interpolated value
     }
+
+    public static float FloatLerp(float posA, float posB, float t, EasingType easingType = 0)
+    {
+        t = Mathf.Clamp01(t);
+        // Apply an easing function to t
+        switch (easingType)
+        {
+            case EasingType.Linear:
+                break; // Linear easing means t stays unchanged
+            case EasingType.EaseIn:
+                t = t * t * t;
+                break;
+            case EasingType.EaseOut:
+                t = 1 - Mathf.Pow((1 - t), 3);
+                break;
+            case EasingType.EaseInOut:
+                if (t < 0.5)
+                {
+                    t = 4 * t * t * t;
+                }
+                else
+                {
+                    t = 1 - Mathf.Pow(-2 * t + 2, 3) / 2;
+                }
+                break;
+        }
+
+        float interpolatedPos = (1 - t) * posA + t * posB;
+        return interpolatedPos;                             //return the interpolated value
+    }
 }
--- MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/UI/SceneLoader.cs
+++ MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/UI/SceneLoader.cs
@@ -8,6 +8,8 @@ public class SceneLoader : MonoBehaviour
 {
     public GameObject loadingScreen;
     public Slider loadingSlider;
+    public float fillSpeed = 1f;
+    public float minLoadTime = 2f;        //no instant loading screen for better ux. 2s should be fine.
 
     public void LoadScene(string name)
     {
@@ -18,12 +20,13 @@ public class SceneLoader : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(name);
         loadingScreen.SetActive(true);
-
-
-        while (!operation.isDone)
+        float elapsedTime = 0f;
+        while ((!operation.isDone) || (elapsedTime < minLoadTime))
         {
+            elapsedTime += Time.deltaTime;
             float progress = Mathf.Clamp01(operation.progress / 0.9f);             // as unity loading only use until 0.9 and use other 0.1 for activation
-            loadingSlider.value = progress;
+            float interPolatedProgress = Interpolation.FloatLerp(loadingSlider.value, progress, fillSpeed);
+            loadingSlider.value = interPolatedProgress;
 
             yield return null;
         }

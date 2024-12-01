53c6167206da3ea737571281577e31cda2f2bc52
7450N <minthawkaung10@gmail.com>
2024-12-02 03:18:30 +0800

    Adding Loading screen
    loading screen done

--- MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/UI/SceneLoader.cs
+++ MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/UI/SceneLoader.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider loadingSlider;
+
     public void LoadScene(string name)
     {
         StartCoroutine(ShowProgress(name));
@@ -13,10 +17,13 @@ public class SceneLoader : MonoBehaviour
     IEnumerator ShowProgress(string name)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(name);
+        loadingScreen.SetActive(true);
+
 
         while (!operation.isDone)
         {
             float progress = Mathf.Clamp01(operation.progress / 0.9f);             // as unity loading only use until 0.9 and use other 0.1 for activation
+            loadingSlider.value = progress;
 
             yield return null;
         }

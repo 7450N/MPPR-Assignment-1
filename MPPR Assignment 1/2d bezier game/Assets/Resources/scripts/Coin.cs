c3a1f488f65c39ad4384930e3e3c66d4076ab575
JaNaw <52690375+janaw-gl@users.noreply.github.com>
2024-12-02 00:50:10 +0800

    Coin collect animation
    Coins move to the coin counter display when collided

--- MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/Coin.cs
+++ MPPR Assignment 1/2d bezier game/Assets/Resources/scripts/Coin.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     public CoinCounter CoinCounter;
+    public Transform target;
+    private float duration = 1f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
             CoinCounter.coins += 1;
             Debug.Log("coins:" + CoinCounter.coins);
+            StartCoroutine(MoveToTarget());
         }
+    }
+    IEnumerator MoveToTarget()
+    {
+        Vector3 startPos = transform.position;
+        Vector3 targetPos = target.position;
+        float elapsedTime = 0;
+
+        while (elapsedTime < duration)
+        {
+            float t = elapsedTime / duration;
+            t = Mathf.Clamp01(t);
+
+            Vector3 newPos = Interpolation.Lerp(startPos, targetPos, t, Interpolation.EasingType.EaseIn);
+            transform.position = newPos;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target.position;
+        Destroy(gameObject);
     }
 
 }
